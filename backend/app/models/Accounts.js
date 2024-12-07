98bec84f9e220ab7e30469a3c09bc82c4f135e67
Lu1s16 <3045905330115@ingenieria.usac.edu.gt>
2024-12-07 15:25:42 -0600

    Add model accounts

---
+++ backend/app/models/Accounts.js
@@ -0,0 +1,40 @@
+import { DataTypes } from "sequelize"
+import sequelize from "../../config/database-connection.js"
+
+
+const AccountModel = sequelize.define("accounts", {
+
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      account_number: {
+        type: DataTypes.CHAR(10),
+        allowNull: false,
+        unique: true,
+      },
+      balance: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        defaultValue: 0.0,
+        validate: {
+          min: 0, // Reemplaza la constraint CHECK en Sequelize
+        },
+      },
+      created_at: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+
+}, {
+    tableName: "accounts",
+    timeStamps: false
+
+})
+
+export default AccountModel
