0da9f06a51148ab2718efd030787b3f1f50861b8
David15Barrera <david1315barrera@gmail.com>
2024-12-07 04:02:56 -0600

    Creating initial admin routes and folders

--- frontayd/src/app/modules/admin/admin.module.ts
+++ frontayd/src/app/modules/admin/admin.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-
-
+import { ReportsComponent } from './reports/reports.component';
+import { UsersComponent } from './users/users.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    ReportsComponent,
+    UsersComponent,
+    DashboardComponent
   ]
 })
 export class AdminModule { }
--- frontayd/src/app/shared/layouts/admin-navbar/admin-navbar.component.ts
+++ frontayd/src/app/shared/layouts/admin-navbar/admin-navbar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-admin-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './admin-navbar.component.html',
   styleUrl: './admin-navbar.component.scss'
 })
 export class AdminNavbarComponent {
+  isSidebarOpen = false;
+  companyName: string = ''; // Variable para almacenar el nombre de la empresa
+
+
+
+  logout() {
+
+  }
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 
 }
