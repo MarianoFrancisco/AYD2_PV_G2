1851b09bc66ab377fddc30c7036640a4a8a67981
Mariano Camposeco <mariano1941@outlook.es>
2024-12-06 19:44:49 -0600

    feat: add database connection base

---
+++ backend/config/database-connection.js
@@ -0,0 +1,40 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+/*
+* @author
+* Mariano Camposeco {@literal (mariano1941@outlook.es)}
+*/
+dotenv.config();
+
+class Database {
+  static instance;
+
+  static getInstance() {
+    if (!Database.instance) {
+      Database.instance = new Sequelize(
+        process.env.MYSQL_DATABASE,
+        process.env.MYSQL_USER,
+        process.env.MYSQL_PASSWORD, {
+          host: process.env.MYSQL_HOST,
+          dialect: 'mysql',
+          port: process.env.MYSQL_PORT,
+          logging: false
+        }
+      );
+    }
+    return Database.instance;
+  }
+}
+
+(async () => {
+  try {
+    const sequelize = Database.getInstance();
+    await sequelize.authenticate();
+    console.log('Connection successfully established with the database.');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+})();
+
+export default Database.getInstance();
