7674f5f5fd878d960e9941c17baafd4ab124a1ca
David15Barrera <david1315barrera@gmail.com>
2024-12-07 15:32:20 -0600

    switching to a single controller component

--- frontayd/src/app/app.routes.ts
+++ frontayd/src/app/app.routes.ts
@@ -1,33 +1,21 @@
 import { Routes } from '@angular/router';
 
-import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { ClientNavbarComponent } from './shared/layouts/client-navbar/client-navbar.component';
-import { AdminNavbarComponent } from './shared/layouts/admin-navbar/admin-navbar.component';
 export const routes: Routes = [
 
-{
-    path:'session',
-    component: AuthLayoutComponent,
-    loadChildren: () => import('./core/auth/auth.module').then(m => m.AuthModule)
-},
 {
     path: 'user',
     component: ClientNavbarComponent,
     loadChildren:() => import('./modules/common-user/common-user.module').then(m => m.CommonUserModule)
 },
 {
-    path: 'admin',
-    component: AdminNavbarComponent,
-    loadChildren:() => import('./modules/admin/admin.module').then(m => m.AdminModule)
-},
-{
     path: "",
-    redirectTo: "session/login",
+    redirectTo: "user/dashboard",
     pathMatch: 'full'
 },
 {
     path: '**',
-    redirectTo: "session/login",
+    redirectTo: "user/dashboard",
     pathMatch: 'full'
 }
 ];
