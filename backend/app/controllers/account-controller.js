d7a6813d425f0dd52d46bab9feb08fe1ec0a21df
Mariano Camposeco <mariano1941@outlook.es>
2024-12-07 22:37:52 -0600

    fix: date update salary and add validateUser

--- backend/app/controllers/account-controller.js
+++ backend/app/controllers/account-controller.js
@@ -1,22 +1,19 @@
 import AccountModel from "../models/account-model.js";
-import sequelize from "../../config/database-connection.js";
-
 
 const getBalance = async (req, res) => {
 
-    const { accountNumber } = req.params
+    const userModel = req.userModel;
 
     try {
-        const bank_accounts = await AccountModel.findOne({
+        const accountModel = await AccountModel.findOne({
             where: {
-                account_number: accountNumber,
+                user_id: userModel.id
             }
-
         })
 
         res.status(200).json({
-            "Saldo": bank_accounts.balance,
-            "Fecha": bank_accounts.created_at
+            "Saldo": accountModel.balance,
+            "Fecha": accountModel.update_balance_at
         });
 
     } catch (error) {
@@ -29,6 +26,3 @@ const getBalance = async (req, res) => {
 export {
     getBalance
 }
-
-
-
