fc14adc6d653bc2212df5af174799682bec28ee2
Mariano Camposeco <mariano1941@outlook.es>
2024-12-07 22:05:38 -0600

    feat: add models for payment services

--- backend/app.js
+++ backend/app.js
@@ -5,20 +5,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import accountRouter from "./app/routes/account.routes.js";
+import paymentServiceRouter from "./app/routes/payment-services.routes.js";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(bodyParser.json({ limit: '15mb' }));
 app.use(cors());
+app.use('/signature', express.static(path.join(__dirname, 'img')));
 
 const api = '/api';
+const showBalance = `${api}/account`;
+const paymentService = `${api}/payment-service`;
 
-const showBalance = `${api}/Account`;
-
-
-
-app.use(showBalance, accountRouter)
+app.use(showBalance, accountRouter);
+app.use(paymentService, paymentServiceRouter);
 
 export default app;
