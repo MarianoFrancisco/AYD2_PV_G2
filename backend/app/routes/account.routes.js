4de0537baef06d056cf7dc1bd6d63fb1c241712e
Mariano Camposeco <mariano1941@outlook.es>
2024-12-21 12:57:48 -0600

    feat: add data to update account

--- backend/app/routes/account.routes.js
+++ backend/app/routes/account.routes.js
@@ -1,8 +1,13 @@
 import express from "express"
-import { getBalance } from "../controllers/account-controller.js"
-import validateUser from '../middleware/validate-user-middleware.js';
+import {
+    getBalance,
+    updateAccountInfo
+} from "../controllers/account-controller.js"
+import validateUserById from '../middleware/validate-user-middleware.js';
 
 const router = express.Router();
-router.get("/show-balance", validateUser('query'), getBalance);
+
+router.get("/show-balance", validateUserById('query'), getBalance);
+router.patch('/update', updateAccountInfo);
 
 export default router;
