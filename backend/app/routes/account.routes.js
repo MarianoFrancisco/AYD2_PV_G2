5280ff730b4c752a39e3352029c05be9c427f960
Lu1s16 <3045905330115@ingenieria.usac.edu.gt>
2024-12-22 17:13:19 -0600

    [add] se agrego crear empleados

--- backend/app/routes/account.routes.js
+++ backend/app/routes/account.routes.js
@@ -5,13 +5,15 @@ import {
     getPhotographyPathByAccountNumber,
     updateAccountInfo,
     createAccount,
-    registroQuejas
+    registroQuejas,
+    createEmployee
 } from "../controllers/account-controller.js"
 import validateUserById from '../middleware/validate-user-middleware.js';
 import imageUpload from '../middleware/image-middleware.js';
 import uploadImageHandler from '../handlers/upload-image-handler.js';
-
-
+import pdfUpload from "../middleware/pdf-middleware.js";
+import uploadPdfhandler from "../handlers/upload-pdf-handler.js";
+import upload from "../middleware/uploadfiles.js";
 /*
  * @author
  * Mariano Camposeco {@literal (mariano1941@outlook.es)}
@@ -23,6 +25,13 @@ router.get('/security-question', getSecurityQuestionByAccountNumber);
 router.get('/photography', getPhotographyPathByAccountNumber);
 router.get("/createAccount", imageUpload.single('photo'), uploadImageHandler, createAccount)
 router.get("/registrarQueja", imageUpload.single('photo'), registroQuejas)
+router.post("/registrarEmpleado", upload.fields([
+    { name: 'photo', maxCount: 1 }, // Campo 'photo', máximo 1 archivo
+    { name: 'pdf', maxCount: 1 }    // Campo 'pdf', máximo 1 archivo
+]),
+uploadImageHandler,
+uploadPdfhandler,
+createEmployee)
 router.patch('/update', imageUpload.single('photo'), uploadImageHandler, updateAccountInfo);
 
 
