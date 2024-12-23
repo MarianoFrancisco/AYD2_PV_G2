5257229f9b77ae5d5964fe02aada58777685a3ff
Mariano Camposeco <mariano1941@outlook.es>
2024-12-23 10:33:59 -0600

    fix: problem with loan payment

--- backend/app/routes/loan-list.routes.js
+++ backend/app/routes/loan-list.routes.js
@@ -4,6 +4,6 @@ import { getLoansByAccount } from '../controllers/loan-list-controller.js';
 const router = express.Router();
 
 // Ruta para obtener préstamos por account_id
-router.get('/:account_id', getLoansByAccount);
+router.get('/:account_number', getLoansByAccount);
 
 export default router;
