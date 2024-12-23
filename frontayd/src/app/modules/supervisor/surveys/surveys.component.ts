4a8ebeaae70ea688a620724804eb841d47f00e39
Cristofher <201700686@ingenieria.usac.edu.gt>
2024-12-23 00:34:22 -0600

    [add]vista de encuestas para supervisor

--- frontayd/src/app/modules/supervisor/surveys/surveys.component.ts
+++ frontayd/src/app/modules/supervisor/surveys/surveys.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Surveys } from '../../../interfaces/surveys';
+import { SurveysService } from '../../../services/surveys/surveys.service';
 
 @Component({
   selector: 'app-surveys',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './surveys.component.html',
   styleUrl: './surveys.component.scss'
 })
-export class SurveysComponent {
+export class SurveysComponent implements OnInit {
+
+  data: Surveys[] = []
+
+  constructor(private surveyService: SurveysService){}
+
+  ngOnInit(): void {
+    this.surveyService.getAllSurveys().subscribe({
+      next: (data) =>{
+        this.data = data
+      },
+      error: () => {
+        console.log("error al obtener registros")
+      }
+    })
+  }
+
+  viewOnlySurveys(data: Surveys) {
+    Swal.fire({
+      icon: 'info',
+      html: `
+            <strong>${data.Question1}</strong><br> ${data.Answer1}<br>
+            <strong>${data.Question2}</strong><br> ${data.Answer2}<br>
+            <strong>${data.Question3}</strong><br> ${data.Answer3}<br>
+            <strong>${data.Question4}</strong><br> ${data.Answer4}<br>
+            <strong>${data.Question4}</strong><br> ${data.Answer5}<br>
+          `,
+      confirmButtonText: 'Aceptar'
+          });
+  }
+
+  convertirFecha(timestamp: number): string {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleDateString();  
+  }
 
 }
