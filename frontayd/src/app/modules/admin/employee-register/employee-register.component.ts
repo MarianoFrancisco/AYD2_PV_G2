8d53c0cd584552dc457efab3545771401f8408db
David15Barrera <david1315barrera@gmail.com>
2024-12-23 14:10:43 -0600

    feat: registrar empleados

--- frontayd/src/app/modules/admin/employee-register/employee-register.component.ts
+++ frontayd/src/app/modules/admin/employee-register/employee-register.component.ts
@@ -1,12 +1,82 @@
 import { Component } from '@angular/core';
+import { EmployeeRegisterService } from '../../services/Empleado.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-employee-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './employee-register.component.html',
   styleUrl: './employee-register.component.scss'
 })
 export class EmployeeRegisterComponent {
+  fullName: string = '';
+  phone: string = '';
+  age: number | null = null;
+  cui: string = '';
+  email: string = '';
+  gender: string = 'Masculino';
+  maritalStatus: string = 'Soltero';
+  photo: File | null = null;
+  pdf: File | null = null;
 
+  constructor(private employeeRegisterService: EmployeeRegisterService) {}
+
+  onFileChange(event: Event, fileType: 'photo' | 'pdf'): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      if (fileType === 'photo') {
+        this.photo = input.files[0];
+      } else if (fileType === 'pdf') {
+        this.pdf = input.files[0];
+      }
+    }
+  }
+
+  onSubmit(): void {
+    if (this.photo && this.pdf) {
+      this.employeeRegisterService
+        .registerEmployee(
+          this.fullName,
+          this.phone,
+          this.age!,
+          this.cui,
+          this.email,
+          this.gender,
+          this.maritalStatus,
+          this.photo,
+          this.pdf
+        )
+        .subscribe({
+          next: (response) => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Empleado creado',
+              text: `${response.message}`,
+              confirmButtonText: 'Aceptar',
+            }).then(() => {
+              window.location.reload();
+            });
+          },
+          error: (err) => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'Error al crear el empleado.',
+              confirmButtonText: 'Aceptar',
+            });
+            console.error(err);
+          }
+        });
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Datos incompletos',
+        text: 'Por favor, sube una foto y un documento PDF.',
+        confirmButtonText: 'Aceptar',
+      });
+    }
+  }
 }
---
+++ frontayd/src/app/modules/services/Empleado.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EmployeeRegisterResponse } from '../../interfaces/empleado';
+import { environment } from '../../../environments/environment';
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeRegisterService {
+  private apiUrl = `${environment.API_URL}/account/registrarEmpleado`;
+
+  constructor(private http: HttpClient) {}
+
+  registerEmployee(
+    fullName: string,
+    phone: string,
+    age: number,
+    cui: string,
+    email: string,
+    gender: string,
+    maritalStatus: string,
+    photo: File,
+    pdf: File
+  ): Observable<EmployeeRegisterResponse> {
+    const formData = new FormData();
+    formData.append('fullName', fullName);
+    formData.append('phone', phone);
+    formData.append('age', age.toString());
+    formData.append('cui', cui);
+    formData.append('email', email);
+    formData.append('gender', gender);
+    formData.append('marital_status', maritalStatus);
+    formData.append('photo', photo);
+    formData.append('pdf', pdf);
+
+    return this.http.post<EmployeeRegisterResponse>(this.apiUrl, formData);
+  }
+}
