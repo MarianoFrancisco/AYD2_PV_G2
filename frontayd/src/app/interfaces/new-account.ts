2a3160ee3eadfef659e681d7e8ae1a15d6d8db72
Cristofher <201700686@ingenieria.usac.edu.gt>
2024-12-22 18:50:54 -0600

    [add]crear cuenta

---
+++ frontayd/src/app/interfaces/new-account.ts
@@ -0,0 +1,14 @@
+export interface NewAccount {
+    firstName: string,
+    lastName: string,
+    cui: string,
+    phone: string,
+    email: string,
+    age: number,
+    gender: string,
+    accountType: string,
+    securityQuestion: string,
+    securityAnswer: string,
+    amount: number,
+    photo64: string,
+}
