1d15e5dbbd0ca305b515dd6c05bfadf204be6bdf
David15Barrera <david1315barrera@gmail.com>
2024-12-23 15:59:47 -0600

    feat: registro de empleado rol

--- frontayd/src/app/modules/services/Empleado.service.ts
+++ frontayd/src/app/modules/services/Empleado.service.ts
@@ -8,10 +8,38 @@ import { environment } from '../../../environments/environment';
 })
 export class EmployeeRegisterService {
   private apiUrl = `${environment.API_URL}/account/registrarEmpleado`;
+  private url = `${environment.API_URL}/account/registrarAdmin`;
 
   constructor(private http: HttpClient) {}
 
   registerEmployee(
+    fullName: string,
+    phone: string,
+    age: number,
+    cui: string,
+    email: string,
+    gender: string,
+    maritalStatus: string,
+    photo: File,
+    pdf: File,
+    role: string, 
+  ): Observable<EmployeeRegisterResponse> {
+    const formData = new FormData();
+    formData.append('fullName', fullName);
+    formData.append('phone', phone);
+    formData.append('age', age.toString());
+    formData.append('cui', cui);
+    formData.append('email', email);
+    formData.append('gender', gender);
+    formData.append('marital_status', maritalStatus);
+    formData.append('photo', photo);
+    formData.append('pdf', pdf);
+    formData.append('role', role);
+
+    return this.http.post<EmployeeRegisterResponse>(this.apiUrl, formData);
+  }
+
+  registerAdmin(
     fullName: string,
     phone: string,
     age: number,
@@ -33,6 +61,6 @@ export class EmployeeRegisterService {
     formData.append('photo', photo);
     formData.append('pdf', pdf);
 
-    return this.http.post<EmployeeRegisterResponse>(this.apiUrl, formData);
+    return this.http.post<EmployeeRegisterResponse>(this.url, formData);
   }
 }
--- frontayd/src/app/modules/supervisor/admin-registry/admin-registry.component.ts
+++ frontayd/src/app/modules/supervisor/admin-registry/admin-registry.component.ts
@@ -1,12 +1,82 @@
+import { EmployeeRegisterComponent } from './../../admin/employee-register/employee-register.component';
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { EmployeeRegisterService } from '../../services/Empleado.service';
 @Component({
   selector: 'app-admin-registry',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule ],
   templateUrl: './admin-registry.component.html',
   styleUrl: './admin-registry.component.scss'
 })
 export class AdminRegistryComponent {
+  fullName: string = '';
+  phone: string = '';
+  age: number | null = null;
+  cui: string = '';
+  email: string = '';
+  gender: string = 'Masculino';
+  maritalStatus: string = 'Soltero';
+  photo: File | null = null;
+  pdf: File | null = null;
 
+  constructor(private employeeRegisterService: EmployeeRegisterService) {}
+
+  onFileChange(event: Event, fileType: 'photo' | 'pdf'): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      if (fileType === 'photo') {
+        this.photo = input.files[0];
+      } else if (fileType === 'pdf') {
+        this.pdf = input.files[0];
+      }
+    }
+  }
+
+  onSubmit(): void {
+    if (this.photo && this.pdf) {
+      this.employeeRegisterService
+        .registerAdmin(
+          this.fullName,
+          this.phone,
+          this.age!,
+          this.cui,
+          this.email,
+          this.gender,
+          this.maritalStatus,
+          this.photo,
+          this.pdf
+        )
+        .subscribe({
+          next: (response) => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Empleado creado',
+              text: `${response.message}`,
+              confirmButtonText: 'Aceptar',
+            }).then(() => {
+              window.location.reload();
+            });
+          },
+          error: (err) => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'Error al crear el empleado.',
+              confirmButtonText: 'Aceptar',
+            });
+            console.error(err);
+          }
+        });
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: 'Datos incompletos',
+        text: 'Por favor, sube una foto y un documento PDF.',
+        confirmButtonText: 'Aceptar',
+      });
+    }
+  }
 }
