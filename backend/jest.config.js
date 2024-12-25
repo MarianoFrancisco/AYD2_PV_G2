5d9bcdeb5dea2e0d59fe95a8203d5f1f0b8c667a
Programming <mariano1941@outlook.es>
2024-12-25 00:05:58 -0600

    feat: add test for withdrawal

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,6 +2,7 @@ export default {
     testTimeout: 10000,
     testEnvironment: 'node',
     moduleFileExtensions: ['js', 'json'],
+    setupFilesAfterEnv: ['./jest.setup.js'],
     transform: {
         '^.+\\.js$': 'babel-jest',
     },
