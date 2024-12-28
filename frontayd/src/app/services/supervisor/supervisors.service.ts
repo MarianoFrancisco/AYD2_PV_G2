e7630a124ffcd5849643d79e380e2345232f6820
Cristofher <201700686@ingenieria.usac.edu.gt>
2024-12-28 12:27:19 -0600

    Cambio de Contraseña
    [update]Cambio de contraseña

--- frontayd/src/app/services/supervisor/supervisors.service.ts
+++ frontayd/src/app/services/supervisor/supervisors.service.ts
@@ -12,18 +12,33 @@ export class SupervisorsService {
 
   constructor(private http: HttpClient) { }
 
+// Obtener todos los empleados
   getAllEmployees():Observable<{empleados : Admins[]}>{
     return  this.http.get<{empleados : Admins[]}>(`${this.urlApi}/account/getEmpleados`)
   }
 
+//Eliminar Empleados
   deleteEmployes(id:number):Observable<string>{
     return this.http.delete<string>(`${this.urlApi}/users/${id}`)
   }
 
-  updatePassEmployee(id:number):Observable<string>{
-    return this.http.delete<string>(`${this.urlApi}/users/${id}`)
-  }
 
+//Cambio de contraseñas
+  updatePassEmployee(user_name:string):Observable<{message : string}>{
+    const formData = new FormData();
+    formData.append('user_name', user_name);
+
+    return this.http.post<{message : string}>(`${this.urlApi}/cancellation//sendRequstChangePassword`, formData)
+  }
+  getSolicitudChangePassword():Observable<{Solicitudes : Admins[]}>{
+    return  this.http.get<{Solicitudes : Admins[]}>(`${this.urlApi}/cancellation/getRequestChangePassword`)
+  }
+  aprobeUpdatePass(user_name:string):Observable<{message : string}>{
+    const formData = new FormData();
+    formData.append('user_name', user_name);
+
+    return this.http.post<{message : string}>(`${this.urlApi}/account/changePassword`, formData)
+  }
 
 
   getSolicitudCancelService():Observable<{empleados : Admins[]}>{
