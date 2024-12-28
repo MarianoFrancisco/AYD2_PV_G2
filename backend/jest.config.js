51639b5fb5a38d3aac1e45bcfe386c9959740dfe
Programming <mariano1941@outlook.es>
2024-12-28 02:02:17 -0600

    feat: run test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,26 +1,45 @@
 export default {
-    testTimeout: 100000,
-    projects: [
-      {
-        displayName: 'unit',
-        testEnvironment: 'node',
-        moduleFileExtensions: ['js', 'json'],
-        setupFiles: ['<rootDir>/jest.setup.unit.js'],
-        transform: {
-          '^.+\\.js$': 'babel-jest',
-        },
-        testMatch: ['**/tests/unit/**/*.test.js'],
+  testTimeout: 100000,
+  projects: [
+    {
+      displayName: 'unit',
+      testEnvironment: 'node',
+      moduleFileExtensions: ['js', 'json'],
+      setupFiles: ['<rootDir>/jest.setup.unit.js'],
+      transform: {
+        '^.+\\.js$': 'babel-jest',
       },
-      {
-        displayName: 'integration',
-        testEnvironment: 'node',
-        moduleFileExtensions: ['js', 'json'],
-        setupFiles: ['<rootDir>/jest.setup.integration.js'],
-        transform: {
-          '^.+\\.js$': 'babel-jest',
-        },
-        testMatch: ['**/tests/integration/**/*.test.js'],
+      testMatch: ['**/tests/unit/**/*.test.js'],
+      reporters: [
+        'default',
+        [
+          'jest-junit',
+          {
+            outputDirectory: './test-reports/unit',
+            outputName: 'junit.xml',
+          },
+        ],
+      ],
+    },
+    {
+      displayName: 'integration',
+      testEnvironment: 'node',
+      moduleFileExtensions: ['js', 'json'],
+      setupFiles: ['<rootDir>/jest.setup.integration.js'],
+      transform: {
+        '^.+\\.js$': 'babel-jest',
       },
-    ],
-  };
-  
+      testMatch: ['**/tests/integration/**/*.test.js'],
+      reporters: [
+        'default',
+        [
+          'jest-junit',
+          {
+            outputDirectory: './test-reports/integration',
+            outputName: 'junit.xml',
+          },
+        ],
+      ],
+    },
+  ],
+};
