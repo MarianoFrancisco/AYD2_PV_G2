f6d4bfaff62d41445efe7eed14174e441158ef7b
Cristofher <201700686@ingenieria.usac.edu.gt>
2024-12-28 21:16:37 -0600

    Cancelar servicios y Filtros
    [update]cancelar servicios y filtros en solicitud de prestamos y tarjetas

--- frontayd/src/app/modules/supervisor/loan-approve-loans/loan-approve-loans.component.ts
+++ frontayd/src/app/modules/supervisor/loan-approve-loans/loan-approve-loans.component.ts
@@ -2,16 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { Prestamos } from '../../../interfaces/aprobaciones';
 import { SupervisorsService } from '../../../services/supervisor/supervisors.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-loan-approve-loans',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './loan-approve-loans.component.html',
   styleUrl: './loan-approve-loans.component.scss'
 })
 export class LoanApproveLoansComponent implements OnInit {
   data: Prestamos[] = []
+  searchFilter: string = '';
 
   constructor(private supervisorServices: SupervisorsService) { }
 
@@ -70,6 +72,13 @@ export class LoanApproveLoansComponent implements OnInit {
     })
   }
 
+  filtered(){
+    if(this.searchFilter === ''){
+      return this.data
+    }else{
+      return this.data.filter((data) => data.status === this.searchFilter)
+    }
+  }
 
   convertirFecha(timestamp: number): string {
     const date = new Date(timestamp * 1000);
